62dbdef4cac9e74ff4b67c3e0d69bed315acba00
Abigail Biak Tha Len Par <129858798+AbigailBiaktha@users.noreply.github.com>
2023-04-18 18:26:49 +0630

    Add files via upload

---
+++ streamlite.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install streamlit
+
+
+# In[1]:
+
+
+import streamlit as st
+import pandas as pd
+st.write("My First Stremlit app")
+data={"sales":[30,40,50,20,10,40]}
+df=pd.DataFrame(data)
+st.line_chart(df)
+
+
+# In[ ]:
+
+
+
+
